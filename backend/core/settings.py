4cfc1999381bef6c85e4517ebe32f181a14ae0b3
Aravinthishu <aravintharavinth7746@gmail.com>
2026-04-20 17:43:51 +0530

    Final Polish: Added WhiteNoise for static files and increased production stability

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +131,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.db.backends.sqlite3", # Default for non-mongo apps
+    },
+    "mongo": {
+        "BACKEND": "django_mongodb_backend",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 CORS_ALLOW_ALL_ORIGINS = True # For development
 
